d9c956d10be566e1288267d0aac9818007b7f591
Mohamed Elarbi <maelarbi@hawkmail.hfcc.edu>
2023-08-20 01:36:16 -0400

    fixing deployment issue

--- app.py
+++ app.py
@@ -6,3 +6,10 @@ app = Flask(__name__,template_folder='templates',static_folder='static')
 @app.route('/')
 def hello():
     return render_template('index.html')
+
+
+if __name__ == '__main__':
+ 
+    # run() method of Flask class runs the application
+    # on the local development server.
+    app.run(debug=True, port=33507)
