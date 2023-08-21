a903eafb17a93b9fd12906ea8724f7676a3026b9
Mohamed Elarbi <maelarbi@hawkmail.hfcc.edu>
2023-08-21 03:49:48 -0400

    Added email section

--- app.py
+++ app.py
@@ -1,22 +1,57 @@
 from flask import Flask, render_template,request
 from flask_cors import CORS
 from models import create_data, get_data
+from flask_mail import Mail, Message
+import smtplib
 import os
 app = Flask(__name__,template_folder='templates',static_folder='static')
 CORS(app)
-
+app.config['TESTING'] = False
 @app.route('/',methods=['GET'])
 def index():
     testimonial=get_data()
     return render_template('index.html',testimonial=testimonial)
 
+@app.route('/contact',methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        email = request.form.get('email')  
+        phone = request.form.get('phone')  
+        comment = request.form.get('comment')
+        
+        # Create and send the email
+        subject = 'New Contact'
+        body = f'Name: {name}\nEmail: {email}\nPhone: {phone}\nComment: {comment}'
+        
+        try:
+            conn = smtplib.SMTP('smtp.gmail.com', 587)
+            conn.ehlo()
+            conn.starttls()
+            conn.login('t30710948@gmail.com', 'aoozlhvtwfbemyqt')
+
+            send = 'elarbi_m@protonmail.com'
+            conn.sendmail('t30710948@gmail.com', send, f'Subject: {subject}\n\n{body}')
+
+            return render_template('contact.html', success=True)
+        except smtplib.SMTPException as e:
+            error_message = f'An error occurred: {e}'
+            return render_template('contact.html', error=error_message)
+
+    return render_template('contact.html',success=False)
+
 @app.route('/testimonial', methods=['GET', 'POST'])
 def testimonial():
-    if request.method=="POST":
-        name =request.form.get('name')
+    success = False  # Default status is not successful
+    if request.method == 'POST':
+        name = request.form.get('name')
         comment = request.form.get('comment')
-        create_data(name,comment)
-    return render_template('testimonial.html')
+        try:
+            create_data(name, comment)
+            success = True  # Update status if data is successfully submitted
+        except Exception as e:
+            print("Error:", e)
+    return render_template('testimonial.html', success=success)
 
 if __name__ == '__main__':
  
